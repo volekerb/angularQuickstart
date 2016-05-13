8165e76330ef98363266e86122a54b6f915ca39b
volekerb <vsevolod.brekelov@gmail.com>
2016-05-13 19:07:38 +0300

    Completed tutorial part -Added routing -Add css, html templates -Moved components to separate files

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,6 +4,11 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  getHero(id: number) {
+    return Promise.resolve(HEROES).then(
+      heroes => heroes.filter(hero => hero.id === id)[0]
+    );
+  }
   getHeroes() {
     return Promise.resolve(HEROES);
   }
